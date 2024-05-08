813bb7f72f009ef1c4b4fde0a476df75258e52a9
DaironChapid <159819179+DaironChapid@users.noreply.github.com>
2024-05-08 13:50:39 -0500

    Add files via upload

---
+++ security_app_2902093/src/main/java/co/edu/sena/security_app_2902093/Application.java
@@ -0,0 +1,15 @@
+package co.edu.sena.security_app_2902093;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
+
+}
